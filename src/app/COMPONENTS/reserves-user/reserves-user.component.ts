5eb0bdfe561273eb5b63fe68237b63c347a35364
Poleth120 <ariaspoleth52@gmail.com>
2023-01-30 15:52:47 -0500

    cambios

--- src/app/COMPONENTS/reserves-user/reserves-user.component.ts
+++ src/app/COMPONENTS/reserves-user/reserves-user.component.ts
@@ -30,9 +30,9 @@ export class ReservesUserComponent {
   @ViewChild(MatSort) sort!: MatSort;
 
   displayedColumns: string[] = [
-    'Id',
+  
     'Lab Name',
-    'Acciones'
+    'Detalles'
   ];
 
 
