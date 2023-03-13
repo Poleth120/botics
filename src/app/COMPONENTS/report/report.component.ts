73929d14025dff36610e265fb8f6a74cd8dd19de
Christian Palacios <cristhianpalacios3@gmail.com>
2023-03-13 08:14:38 -0500

    Auth guard.

--- src/app/COMPONENTS/report/report.component.ts
+++ src/app/COMPONENTS/report/report.component.ts
@@ -2,21 +2,19 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
-
-import * as pdfMake from "pdfmake/build/pdfmake";
-import * as pdfFonts from "pdfmake/build/vfs_fonts";
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 import { AdminService } from 'src/app/services/admin.service';
-const htmlToPdfmake = require("html-to-pdfmake");
+const htmlToPdfmake = require('html-to-pdfmake');
 (pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrls: ['./report.component.css']
+  styleUrls: ['./report.component.css'],
 })
 export class ReportComponent {
-
-reportD:any;
+  reportD: any;
 
   constructor(private adminService: AdminService) {}
 
@@ -38,68 +36,94 @@ reportD:any;
     'Host Name',
     'Serial del CPU',
     'Serial del Monitor',
-    "Modelo",
-    "Procesador",
-    "Ram",
-    "Disco duro"
+    'Modelo',
+    'Procesador',
+    'Ram',
+    'Disco duro',
   ];
 
   displayedColumnsH: string[] = [
     'Host Name',
     'Laboratorio',
     'Fecha',
-    'Detalle'
+    'Detalle',
   ];
 
   ngOnInit() {
     this.adminService.historyIndex().subscribe((data) => {
-     this.history = new MatTableDataSource<any>(data);
-    })
+      this.history = new MatTableDataSource<any>(data);
+    });
     this.adminService.computerIndex().subscribe((data) => {
       this.computers = new MatTableDataSource<any>(data);
-    })
+    });
   }
 
   now = new Date();
   public async downloadAsPDFC() {
     const pdfTable = this.pdfTableC.nativeElement;
     var html = htmlToPdfmake(pdfTable.innerHTML);
-    const documentDefinition = { content: [{image: await this.getBase64ImageFromURL("../assets/img/reporte-compu.png"), fit:[520,520]}, {text: 'Fecha: ' + this.now}, html]};
-    pdfMake.createPdf(documentDefinition).download('reporte-computadoras-'+this.now+'.pdf');
+    const documentDefinition = {
+      content: [
+        {
+          image: await this.getBase64ImageFromURL(
+            '../assets/img/reporte-compu.png'
+          ),
+          fit: [520, 520],
+        },
+        { text: 'Fecha: ' + this.now },
+        html,
+      ],
+    };
+    pdfMake
+      .createPdf(documentDefinition)
+      .download('reporte-computadoras-' + this.now + '.pdf');
   }
 
   public async downloadAsPDFH() {
     const pdfTable = this.pdfTableH.nativeElement;
     var html = htmlToPdfmake(pdfTable.innerHTML);
-    const documentDefinition = { content: [{image: await this.getBase64ImageFromURL("../assets/img/reporte-hist.png"), fit:[520,520]}, {text: 'Fecha: ' + this.now}, html]};
-    pdfMake.createPdf(documentDefinition).download('reporte-historial-'+this.now+'.pdf');
+    const documentDefinition = {
+      content: [
+        {
+          image: await this.getBase64ImageFromURL(
+            '../assets/img/reporte-hist.png'
+          ),
+          fit: [520, 520],
+        },
+        { text: 'Fecha: ' + this.now },
+        html,
+      ],
+    };
+    pdfMake
+      .createPdf(documentDefinition)
+      .download('reporte-historial-' + this.now + '.pdf');
   }
 
   getBase64ImageFromURL(url: string) {
-  return new Promise((resolve, reject) => {
-    var img = new Image();
-    img.setAttribute("crossOrigin", "anonymous");
+    return new Promise((resolve, reject) => {
+      var img = new Image();
+      img.setAttribute('crossOrigin', 'anonymous');
 
-    img.onload = () => {
-      var canvas = document.createElement("canvas");
-      canvas.width = img.width;
-      canvas.height = img.height;
+      img.onload = () => {
+        var canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
 
-      var ctx = canvas.getContext("2d");
-      ctx?.drawImage(img, 0, 0);
+        var ctx = canvas.getContext('2d');
+        ctx?.drawImage(img, 0, 0);
 
-      var dataURL = canvas.toDataURL("image/png");
+        var dataURL = canvas.toDataURL('image/png');
 
-      resolve(dataURL);
-    };
+        resolve(dataURL);
+      };
 
-    img.onerror = error => {
-      reject(error);
-    };
+      img.onerror = (error) => {
+        reject(error);
+      };
 
-    img.src = url;
-  });
-}
+      img.src = url;
+    });
+  }
 
   filterComputers(searchTerm: string) {
     this.computers.filter = searchTerm.trim().toLocaleLowerCase();
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TokenStorageService } from '../services/token-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(public tokenService: TokenStorageService, public router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      console.log(JSON.stringify(this.tokenService.getUser()))
+      if (!this.tokenService.getUser()?.jwtToken) {
+        this.router.navigate(['login']);
+        return false;
+      }
+    return true;
+  }
+
+}
